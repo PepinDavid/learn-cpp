021331770ebb7351f211c1a5cc4b04f7dabe408e
David <david@live.fr>
2017-01-13 16:21:58 +0100

    ajout d'une fenetre 'QtWidget'

--- qt/test/test/main.cpp
+++ qt/test/test/main.cpp
@@ -3,8 +3,17 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QPushButton btn("Salut vous !");
-    btn.show();
+    //creation d'une fenetre
+    QWidget window;
+    window.setFixedSize(300, 150);
+    //creation d'un bouton, ayant pour parent la fenetre
+    QPushButton btn("Salut vous !", &window);
+    btn.move(60, 50);
+    btn.setFont(QFont("Comic Sans MS", 14));
+    btn.setCursor(Qt::PointingHandCursor);
+    btn.setToolTip("Hello");
+
+    window.show();
 
     return app.exec();
 }
