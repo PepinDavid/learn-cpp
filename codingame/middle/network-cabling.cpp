8f0ab59dc83b804dcb34903e978dcee4bcb94ee8
David <david@live.fr>
2017-02-14 12:42:35 +0100

    modification stockage des maison et calcule avg Y et reussite a 100%

--- codingame/middle/network-cabling.cpp
+++ codingame/middle/network-cabling.cpp
@@ -1,75 +1,53 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-/******************
+typedef pair<int, int> points;
 
-83% de réussite, pas fini
-***************/
-
-//structure position des maisons
-struct home{
-    int x;
-    int y;
-};
-//calcule de la moyenne des points sur Y
-double getAvgY(home * h, int& N){
-    double avgY = 0.0;
-    for (int i = 0; i < N; i++) {
-        avgY += h[i].y;
-    }
-    cerr << "avgY " << avgY << endl;
-    cerr << "avgY/N " << avgY/N << endl;
-    return avgY/N;
-}
-long bestLengthY(home * h, int& N, double& avg){
-    long lengthMin = 0;
-    long lengthMax = 0;
-    long lengthMid = 0;
-    for (int i = 0; i < N; i++) {
-        lengthMin += abs(h[i].y - (avg-1));
-        lengthMid += abs(h[i].y - avg);
-        lengthMax += abs(h[i].y - (avg+1));
-    }
-    cerr << "lengthMin " << lengthMin << endl;
-    cerr << "lengthMid " << lengthMid << endl;
-    cerr << "lengthMax " << lengthMax << endl;
-    return  min(min(lengthMax, lengthMin),lengthMid);
-}
 int main()
 {
+    vector<points> positions;
+    
     int N;
-    int total = 0;
     cin >> N; cin.ignore();
-    home * h = new home[N];
     for (int i = 0; i < N; i++) {
         int X;
         int Y;
-        cin >> X >> Y; cin.ignore();
-        h[i].x = X;
-        h[i].y = Y;
+        cin >> X >> Y; cin.ignore();        
+        positions.push_back({X, Y});
     }
-    //recherche du max et min X pour la difference pour la distance
-    //du cable sur l'axe X
-    int minX = h[0].x;
-    int maxX = h[0].x;
-    for(int i = 1; i < N ; i++){
-        if(minX > h[i].x)
-            minX = h[i].x;
-        if(maxX < h[i].x)
-            maxX = h[i].x;
+
+    //calcule avg de Y
+    //on trie dans le sens croissant
+    sort(positions.begin(), positions.end(), [](points p1, points p2){ return p1.second < p2.second; });
+    int mainLengthY;
+    //si taille vector mod 2 != 0
+    if (positions.size() % 2)
+        mainLengthY = positions[positions.size() / 2].second;
+    else //taille vector mod 2 == 0
+        mainLengthY = (positions[(positions.size() / 2)-1].second + positions[positions.size() / 2].second) / 2;
+    
+    cerr << mainLengthY << endl;
+    // Calcule de avg de X
+    int minPosX = positions[0].first;
+    int maxPosX = positions[0].first;
+    for (points& pos : positions)
+    {
+        if (pos.first < minPosX)
+            minPosX = pos.first;
+        if (pos.first > maxPosX)
+            maxPosX = pos.first;
     }
-    cerr << minX << endl;
-    cerr << maxX << endl;
-    //calcule de la moyenne des cables sur Y
-    double avg = floor(getAvgY(h, N));
-    cerr << avg << endl;
-    //calcule de plusieurs longueur possible
-    //le nombre en sorti devant etre un entier
-    //et la moyenne est un double on prend donc
-    //plusieurs possibilité a +/- 1
-    long lengthMin = bestLengthY(h, N, avg);
-    cout << (maxX-minX)+lengthMin << endl;
+    
+    int mainLengthX = maxPosX - minPosX;
+
+    // Calcul taille total du cable principal
+    long long total = mainLengthX;
+    for (points& pos : positions)
+        total += abs(mainLengthY - pos.second);
+    
+    cout << total << endl;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -7,41 +7,53 @@ using namespace std;
 
 int main()
 {
-    string  testString  = "Hello world! \"ça\" Gaz Gégé", autreSring = "j\'ai : ";
-    int     age         = 41;
-    double a(42), b(42);
-    string& refTestString = testString; //accées a testString par référence évite l'utilisation
-    //de mémoire en plus
-    string nameSurname(""), city("");
+//    string  testString  = "Hello world! \"ça\" Gaz Gégé", autreSring = "j\'ai : ";
+//    int     age         = 41;
+//    double a(42), b(42);
+//    string& refTestString = testString; //accées a testString par référence évite l'utilisation
+//    //de mémoire en plus
+//    string nameSurname(""), city("");
+//
+//    cout << testString << endl;
+//    cout << autreSring << age << "ans" << endl;
+//    cout << "test accés variable par référence : " << refTestString << endl;
+//    cout << "Quel est votre nom et votre prénom ? " << endl;
+//    getline(cin,nameSurname); //fonction qui sert a l'acquisition d'une phrase
+//    cout << "Vous vous appelez : " << nameSurname << "\n";
+//    cout << "Quel age avez vous ?" << endl;
+//    cin >> age;
+//    //quand on fait l'acquisition d'une variable number si on veut faire l'acquisition
+//    //d'une variables de type STRING a l'aide de la fonction getline, il faut un cin.ignore()
+//    cin.ignore();
+//    cout << "Vous avez " << age << " ans " << endl;
+//    cout << "Où habitez vous ? " << endl;
+//    getline(cin, city);
+//    cout << "Vous donc a : " << city << endl;
+//    cout << "Addition !!! " << endl;
+//    cout << "Ecrire le premier nombre à mettre dans l'addition" << endl;
+//    cin >> a;
+//    cout << "Ecrire le deuxieme nombre : " << endl;
+//    cin >> b;
+//    cout << " Addition de " << a << " + " << b << " = " << a+b << endl;
+//    cout << "Comment vous appelez vous au fait ?" << endl;
+//    cin.ignore();
+//    getline(cin,testString);
+//    cout << "Bonjour " << testString << endl;
+//    cout << "Ecrire un nombre auquel on ajoutera deux : ";
+//    cin >> a;
+//    b = addTwo(a);
+//    cout << " le nombre original : " << a << ". Qui fait donc : " << b;
 
-    cout << testString << endl;
-    cout << autreSring << age << "ans" << endl;
-    cout << "test accés variable par référence : " << refTestString << endl;
-    cout << "Quel est votre nom et votre prénom ? " << endl;
-    getline(cin,nameSurname); //fonction qui sert a l'acquisition d'une phrase
-    cout << "Vous vous appelez : " << nameSurname << "\n";
-    cout << "Quel age avez vous ?" << endl;
-    cin >> age;
-    //quand on fait l'acquisition d'une variable number si on veut faire l'acquisition
-    //d'une variables de type STRING a l'aide de la fonction getline, il faut un cin.ignore()
-    cin.ignore();
-    cout << "Vous avez " << age << " ans " << endl;
-    cout << "Où habitez vous ? " << endl;
-    getline(cin, city);
-    cout << "Vous donc a : " << city << endl;
-    cout << "Addition !!! " << endl;
-    cout << "Ecrire le premier nombre à mettre dans l'addition" << endl;
-    cin >> a;
-    cout << "Ecrire le deuxieme nombre : " << endl;
-    cin >> b;
-    cout << " Addition de " << a << " + " << b << " = " << a+b << endl;
-    cout << "Comment vous appelez vous au fait ?" << endl;
-    cin.ignore();
-    getline(cin,testString);
-    cout << "Bonjour " << testString << endl;
-    cout << "Ecrire un nombre auquel on ajoutera deux : ";
-    cin >> a;
-    b = addTwo(a);
-    cout << " le nombre original : " << a << ". Qui fait donc : " << b;
+	int * ptr;
+	ptr = new int[3];
+	for(int i = 0; i < 3; i++){
+		ptr[i] = i+3;
+	}
+	ptr = new int [4];
+	ptr[3] = 42;
+	
+	for(int i = 0; i < 4; i++){
+		cout << ptr[i] << endl;
+	} 
     return 0;
 }
