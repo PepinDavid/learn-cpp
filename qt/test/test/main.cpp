7717ff02ed5c09f84eb7743e3fab2e665bba12fa
David <david@live.fr>
2017-03-06 10:47:50 +0100

    remove folder codingame because create repository

--- qt/test/test/main.cpp
+++ qt/test/test/main.cpp
@@ -5,11 +5,14 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     //creation d'une fenetre
     QWidget window;
-    window.setFixedSize(300, 150);
+    window.setFixedSize(500, 250);
     //creation d'un bouton, ayant pour parent la fenetre
     QPushButton btn("Salut vous !", &window);
+    QLabel lbl("Salut toi", &window);
+    lbl.move(75,0);
+    lbl.setFont(QFont("Courrier", 12, QFont::Bold, true));
     btn.move(60, 50);
-    btn.setFont(QFont("Comic Sans MS", 14));
+    btn.setFont(QFont("Comic Sans MS", 20, QFont::Bold, true));
     btn.setCursor(Qt::PointingHandCursor);
     btn.setToolTip("Hello");
 
