d4407531fded2d073fb14108a24af2b597c2bff6
root <root@localhost.localdomain>
2017-05-05 22:03:20 +0200

    for merge

--- qt/tutorial/build-t10-Desktop-Debug/moc_cannonfield.cpp
+++ qt/tutorial/build-t10-Desktop-Debug/moc_cannonfield.cpp
@@ -1,55 +1,25 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'cannonfield.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.7)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include "../t10/cannonfield.h"
-#include <QtCore/qbytearray.h>
-#include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'cannonfield.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.2.1. It"
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.7. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-struct qt_meta_stringdata_CannonField_t {
-    QByteArrayData data[10];
-    char stringdata[88];
-};
-#define QT_MOC_LITERAL(idx, ofs, len) \
-    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    offsetof(qt_meta_stringdata_CannonField_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData) \
-    )
-static const qt_meta_stringdata_CannonField_t qt_meta_stringdata_CannonField = {
-    {
-QT_MOC_LITERAL(0, 0, 11),
-QT_MOC_LITERAL(1, 12, 12),
-QT_MOC_LITERAL(2, 25, 0),
-QT_MOC_LITERAL(3, 26, 8),
-QT_MOC_LITERAL(4, 35, 12),
-QT_MOC_LITERAL(5, 48, 8),
-QT_MOC_LITERAL(6, 57, 8),
-QT_MOC_LITERAL(7, 66, 5),
-QT_MOC_LITERAL(8, 72, 8),
-QT_MOC_LITERAL(9, 81, 5)
-    },
-    "CannonField\0angleChanged\0\0newAngle\0"
-    "forceChanged\0newForce\0setAngle\0angle\0"
-    "setForce\0force\0"
-};
-#undef QT_MOC_LITERAL
-
 static const uint qt_meta_data_CannonField[] = {
 
  // content:
-       7,       // revision
+       6,       // revision
        0,       // classname
        0,    0, // classinfo
        4,   14, // methods
@@ -59,28 +29,27 @@ static const uint qt_meta_data_CannonField[] = {
        0,       // flags
        2,       // signalCount
 
- // signals: name, argc, parameters, tag, flags
-       1,    1,   34,    2, 0x06,
-       4,    1,   37,    2, 0x06,
+ // signals: signature, parameters, type, tag, flags
+      22,   13,   12,   12, 0x05,
+      49,   40,   12,   12, 0x05,
 
- // slots: name, argc, parameters, tag, flags
-       6,    1,   40,    2, 0x0a,
-       8,    1,   43,    2, 0x0a,
-
- // signals: parameters
-    QMetaType::Void, QMetaType::Int,    3,
-    QMetaType::Void, QMetaType::Int,    5,
-
- // slots: parameters
-    QMetaType::Void, QMetaType::Int,    7,
-    QMetaType::Void, QMetaType::Int,    9,
+ // slots: signature, parameters, type, tag, flags
+      73,   67,   12,   12, 0x0a,
+      93,   87,   12,   12, 0x0a,
 
        0        // eod
+};
+
+static const char qt_meta_stringdata_CannonField[] = {
+    "CannonField\0\0newAngle\0angleChanged(int)\0"
+    "newForce\0forceChanged(int)\0angle\0"
+    "setAngle(int)\0force\0setForce(int)\0"
 };
 
 void CannonField::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
         CannonField *_t = static_cast<CannonField *>(_o);
         switch (_id) {
         case 0: _t->angleChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
@@ -89,39 +58,31 @@ void CannonField::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 3: _t->setForce((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
-    } else if (_c == QMetaObject::IndexOfMethod) {
-        int *result = reinterpret_cast<int *>(_a[0]);
-        void **func = reinterpret_cast<void **>(_a[1]);
-        {
-            typedef void (CannonField::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&CannonField::angleChanged)) {
-                *result = 0;
-            }
-        }
-        {
-            typedef void (CannonField::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&CannonField::forceChanged)) {
-                *result = 1;
-            }
-        }
     }
 }
 
-const QMetaObject CannonField::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_CannonField.data,
-      qt_meta_data_CannonField,  qt_static_metacall, 0, 0}
+const QMetaObjectExtraData CannonField::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
 };
 
+const QMetaObject CannonField::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_CannonField,
+      qt_meta_data_CannonField, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &CannonField::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
 
 const QMetaObject *CannonField::metaObject() const
 {
-    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *CannonField::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_CannonField.stringdata))
+    if (!strcmp(_clname, qt_meta_stringdata_CannonField))
         return static_cast<void*>(const_cast< CannonField*>(this));
     return QWidget::qt_metacast(_clname);
 }
@@ -134,10 +95,6 @@ int CannonField::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_c == QMetaObject::InvokeMetaMethod) {
         if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
-    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
-            *reinterpret_cast<int*>(_a[0]) = -1;
         _id -= 4;
     }
     return _id;
--- qt/tutorial/build-t10-Desktop-Debug/moc_lcdrange.cpp
+++ qt/tutorial/build-t10-Desktop-Debug/moc_lcdrange.cpp
@@ -1,53 +1,25 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'lcdrange.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.7)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include "../t10/lcdrange.h"
-#include <QtCore/qbytearray.h>
-#include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'lcdrange.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.2.1. It"
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.7. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-struct qt_meta_stringdata_LCDRange_t {
-    QByteArrayData data[9];
-    char stringdata[71];
-};
-#define QT_MOC_LITERAL(idx, ofs, len) \
-    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    offsetof(qt_meta_stringdata_LCDRange_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData) \
-    )
-static const qt_meta_stringdata_LCDRange_t qt_meta_stringdata_LCDRange = {
-    {
-QT_MOC_LITERAL(0, 0, 8),
-QT_MOC_LITERAL(1, 9, 12),
-QT_MOC_LITERAL(2, 22, 0),
-QT_MOC_LITERAL(3, 23, 8),
-QT_MOC_LITERAL(4, 32, 8),
-QT_MOC_LITERAL(5, 41, 5),
-QT_MOC_LITERAL(6, 47, 8),
-QT_MOC_LITERAL(7, 56, 6),
-QT_MOC_LITERAL(8, 63, 6)
-    },
-    "LCDRange\0valueChanged\0\0newValue\0"
-    "setValue\0value\0setRange\0minVal\0maxVal\0"
-};
-#undef QT_MOC_LITERAL
-
 static const uint qt_meta_data_LCDRange[] = {
 
  // content:
-       7,       // revision
+       6,       // revision
        0,       // classname
        0,    0, // classinfo
        3,   14, // methods
@@ -57,26 +29,26 @@ static const uint qt_meta_data_LCDRange[] = {
        0,       // flags
        1,       // signalCount
 
- // signals: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x06,
+ // signals: signature, parameters, type, tag, flags
+      19,   10,    9,    9, 0x05,
 
- // slots: name, argc, parameters, tag, flags
-       4,    1,   32,    2, 0x0a,
-       6,    2,   35,    2, 0x0a,
-
- // signals: parameters
-    QMetaType::Void, QMetaType::Int,    3,
-
- // slots: parameters
-    QMetaType::Void, QMetaType::Int,    5,
-    QMetaType::Void, QMetaType::Int, QMetaType::Int,    7,    8,
+ // slots: signature, parameters, type, tag, flags
+      43,   37,    9,    9, 0x0a,
+      71,   57,    9,    9, 0x0a,
 
        0        // eod
+};
+
+static const char qt_meta_stringdata_LCDRange[] = {
+    "LCDRange\0\0newValue\0valueChanged(int)\0"
+    "value\0setValue(int)\0minVal,maxVal\0"
+    "setRange(int,int)\0"
 };
 
 void LCDRange::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
         LCDRange *_t = static_cast<LCDRange *>(_o);
         switch (_id) {
         case 0: _t->valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
@@ -84,33 +56,31 @@ void LCDRange::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         case 2: _t->setRange((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
         default: ;
         }
-    } else if (_c == QMetaObject::IndexOfMethod) {
-        int *result = reinterpret_cast<int *>(_a[0]);
-        void **func = reinterpret_cast<void **>(_a[1]);
-        {
-            typedef void (LCDRange::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&LCDRange::valueChanged)) {
-                *result = 0;
-            }
-        }
     }
 }
 
-const QMetaObject LCDRange::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_LCDRange.data,
-      qt_meta_data_LCDRange,  qt_static_metacall, 0, 0}
+const QMetaObjectExtraData LCDRange::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
 };
 
+const QMetaObject LCDRange::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_LCDRange,
+      qt_meta_data_LCDRange, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &LCDRange::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
 
 const QMetaObject *LCDRange::metaObject() const
 {
-    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *LCDRange::qt_metacast(const char *_clname)
 {
     if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_LCDRange.stringdata))
+    if (!strcmp(_clname, qt_meta_stringdata_LCDRange))
         return static_cast<void*>(const_cast< LCDRange*>(this));
     return QWidget::qt_metacast(_clname);
 }
@@ -123,10 +93,6 @@ int LCDRange::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_c == QMetaObject::InvokeMetaMethod) {
         if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
-    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
-            *reinterpret_cast<int*>(_a[0]) = -1;
         _id -= 3;
     }
     return _id;
