a23d72a4fcd2bb02ed2c5081a081dacd5d6b9102
David <david@live.fr>
2018-10-01 15:43:52 +0200

    add little script for create html/js files meteor

---
+++ FileGenerator/JSFileGenerator/Template.js
@@ -0,0 +1,29 @@
+/**********************************************
+
+	{{tpl}}.js    {{Name}} 			   {{date}}
+
+
+**********************************************/
+
+import { Meteor }           from    'meteor/meteor';
+import { Template }         from    'meteor/templating';
+
+import 								'/client/imports/ui/{{tpl}}.html';
+//---------------------------------------------
+
+
+Template.{{tpl}}.onCreate(function(){
+	var self = this;
+});
+
+Template.{{tpl}}.helpers({
+	
+});
+
+Template.{{tpl}}.onRendered({
+	
+});
+
+Template.{{tpl}}.events({
+	
+});
